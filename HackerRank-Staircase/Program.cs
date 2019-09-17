ffd6d56701d3dbf854f110b8da16dee79c754414
jaboud <jaboud94@gmail.com>
2019-09-17 16:25:40 +1000

    Project solution creation

---
+++ HackerRank-Staircase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
