74facba60fe4a943bde475c883b18a4c9b07c822
jaboud <jaboud94@gmail.com>
2019-09-17 17:19:52 +1000

    Created for loop and nested for loop that checks if the value in the column (x) is equal to or greater than the value in the row (y) then it will print out '#'.

--- HackerRank-Staircase/Program.cs
+++ HackerRank-Staircase/Program.cs
@@ -6,7 +6,23 @@ namespace HackerRank_Staircase
     {
         static void Staircase(int n)
         {
-            
+             //For loop starting with the rows which 'y' represents 
+            for (int y = 0; y < n; y++)
+            {
+                //Nested for loop starting with the columns which 'x' represents  
+                for (int x = 0; x < n; x++)
+                {
+                    //Condition statement where we check if the value in the column (x) is equal to or greater than the value in the row (y) then it will print out '#'
+                    if (x >= y)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
         }
         static void Main(string[] args)
         {
